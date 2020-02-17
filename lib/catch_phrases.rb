7aa3623b65ff86c6716c316c7ca55edf92e82b4e
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-17 23:00:56 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,19 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+
+  phase = "It's-a me, Mario!"
+  puts phase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link 
+  phase = "It's Dangerous To Go Alone! Take This."
+  puts phase
+end
+
+def any_phrase(phrase)
+  puts phrase
 end
